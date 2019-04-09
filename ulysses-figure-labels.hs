28df2d07225049e4456a045a411a68dc19561990
Thomas Mailund <mailund@birc.au.dk>
2019-04-09 06:25:52 +0200

    playing with it

--- ulysses-figure-labels.hs
+++ ulysses-figure-labels.hs
@@ -3,9 +3,10 @@ import Text.Pandoc.JSON
 main:: IO()
 main = toJSONFilter extractFigLabel
     where
-    extractFigLabel (Image (id, classes, keyValPairs)
-                     caption (url, title)) =
-        Image (title, classes, keyValPairs) caption (url,[])
+        -- FIXME: I don't know what the options are
+    extractFigLabel (Image (id, classes, options)
+                     caption (url, title)) | id == "" =
+        Image (title, classes, options) caption (url,[])
     extractFigLabel x = x
 
 
